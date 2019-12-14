6ec09776263d5ee4f4368a42ee1415724e49287e
zhouxingjing <zxj5017@163.com>
2019-12-14 11:16:28 +0800

    p17 modify

--- volatile/P17_AtomicReferenceDemo/src/comac/AtomicReferenceDemo.java
+++ volatile/P17_AtomicReferenceDemo/src/comac/AtomicReferenceDemo.java
@@ -1,5 +1,24 @@
 package comac;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 public class AtomicReferenceDemo {
+    static class User{
+      String name;
+      Integer age;
+      public User(String name, Integer age) {
+          this.name = name;
+          this.age = age;
+      }
+    }
+
+    public static void main(String args[]){
+        AtomicReference<User> atomicReference = new AtomicReference<>();
+        User zh3 =new User("zhangsan",10);
+        User li4 = new User("Lisi",11);
+        atomicReference.set(zh3);
+        System.out.println(atomicReference.compareAndSet(zh3,li4) +"\t" + atomicReference.get().toString());
+        System.out.println(atomicReference.compareAndSet(zh3,li4) +"\t" + atomicReference.get().toString());
+    }
 
 }
