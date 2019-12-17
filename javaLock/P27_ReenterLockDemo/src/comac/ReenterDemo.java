7ff8d18416c86f2cefcbebed78f21ba319726e93
zhouxingjing <zxj5017@163.com>
2019-12-17 22:47:46 +0800

    Reenter commit

---
+++ javaLock/P27_ReenterLockDemo/src/comac/ReenterDemo.java
@@ -0,0 +1,30 @@
+package comac;
+
+class Phone{
+    public synchronized void sendSMS() throws Exception{
+        System.out.println(Thread.currentThread().getName()+"\t invoked sendSMS");
+        sendEmail();
+    }
+    public synchronized void sendEmail() throws Exception{
+        System.out.println(Thread.currentThread().getName()+"\t ###### invoked sendEmail");
+    }
+}
+public class ReenterDemo {
+    public static void main(String args[]){
+        Phone phone = new Phone();
+        new Thread(()->{
+            try {
+                phone.sendSMS();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        },"t1").start();
+        new Thread(()->{
+            try {
+                phone.sendSMS();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        },"t2").start();
+    }
+}
