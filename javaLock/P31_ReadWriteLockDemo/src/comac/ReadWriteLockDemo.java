bf9d552387832ff5ec55548962fa60bb8393c3af
zhouxingjing <zxj5017@163.com>
2019-12-23 00:07:41 +0800

    P31 readwriteLockDemo

--- javaLock/P31_ReadWriteLockDemo/src/comac/ReadWriteLockDemo.java
+++ javaLock/P31_ReadWriteLockDemo/src/comac/ReadWriteLockDemo.java
@@ -5,6 +5,13 @@ import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
+/**
+ * 读写锁实例，读是共享锁，写是独占锁
+ * 读时为保证并发量，多线程可同时读，写时其他线程不可对其再读或者写
+ * 读写能共存
+ * 读写不能共存
+ * 写写不能共存
+ */
 public class ReadWriteLockDemo {
     private volatile Map<String,Object> map = new HashMap<>();
     ReentrantReadWriteLock reentrantReadWriteLock = new ReentrantReadWriteLock();
@@ -12,23 +19,25 @@ public class ReadWriteLockDemo {
     public void set(String key,Object value){
         reentrantReadWriteLock.writeLock().lock();
         try {
-            System.out.println("开始写入"+key);
+            System.out.println(Thread.currentThread().getName()+"\t开始写入"+key);
             map.put(key,value);
             try {
-                TimeUnit.SECONDS.sleep(1);}catch (Exception e){e.printStackTrace();}
-            System.out.println("写入完成");
+                TimeUnit.MILLISECONDS.sleep(300);}catch (Exception e){e.printStackTrace();}
+            System.out.println(Thread.currentThread().getName()+"\t写入完成");
         }catch (Exception e){
             e.printStackTrace();
         }finally {
             reentrantReadWriteLock.writeLock().unlock();
         }
     }
-    public void get(Integer key){
+    public void get(String key){
         reentrantReadWriteLock.readLock().lock();
         try {
-            System.out.println("开始读入");
+            System.out.println(Thread.currentThread().getName()+"\t开始读入");
             Object result = map.get(key);
-            System.out.println("读入完成"+result);
+            try {
+                TimeUnit.MILLISECONDS.sleep(300);}catch (Exception e){e.printStackTrace();}
+            System.out.println(Thread.currentThread().getName()+"\t读入完成"+result);
         }catch (Exception e){
             e.printStackTrace();
         }finally {
@@ -40,12 +49,16 @@ public class ReadWriteLockDemo {
         ReadWriteLockDemo readWriteLockDemo = new ReadWriteLockDemo();
 
         for (int i =1;i<=5;i++){
-            Integer temp = 1;
-            new Thread(()->{
+            Integer temp = i;
+                new Thread(()->{
                 readWriteLockDemo.set(temp+"",temp+"");
+            },String.valueOf(temp)).start();
+        }
+        for (int i =1;i<=5;i++){
+            Integer temp = i;
+            new Thread(()->{
                 readWriteLockDemo.get(temp+"");
             },String.valueOf(temp)).start();
-
         }
     }
 }
